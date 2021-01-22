176e709b9594cb25346f2a7dafbcd00d9e3e057c
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-01-22 21:40:32 +0000

    lec4

--- lec4.py
+++ lec4.py
@@ -5,8 +5,8 @@ lec 4 dict and tuple
 # my_tuple = 'a','b','c','d','e'
 # print(my_tuple)
 
-# my_second_tuple = (my_tuple)
-# print(my_second_tuple)
+# my_2nd_tuple = ('a','b','c','d','e')
+# print(my_2nd_tuple)
 
 # not_a_tuple = ('a')
 # print(type(not_a_tuple))
