fbe2e5608b40969b4056fa88c6696c5921a52655
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-01-22 22:08:16 +0000

    lab4 dict and tuple

---
+++ lab4.py
@@ -0,0 +1,47 @@
+"""
+lab4 dict and tuple
+"""
+
+# 3.1
+my_dict = {
+            'name':'Tom',
+            'id':123
+        }
+
+print(my_dict)
+
+# 3.2
+print(my_dict.values())
+print(my_dict.keys())
+
+# 3.3
+my_dict['id']=321
+print(my_dict)
+
+# 3.4
+my_dict.pop('name',None)
+print(my_dict)
+
+# 3.5
+my_tweet = {
+            "tweet_id":1138,
+            "coordinates":(-75,40),
+            "visited_countries":["GR","HK","MY"]
+            }
+            
+print(my_tweet)
+
+# 3.6
+print(len(my_tweet["visited_countries"]))
+
+# 3.7
+my_tweet["visited_countries"].append("CH")
+print(my_tweet)
+
+# 3.8
+print("US" in my_tweet["visited_countries"])
+
+# 3.9
+# (-82,45)
+my_tweet["coordinates"]=(-81,45)
+print(my_tweet)
