26db26b3886e9220d8060a0d4650c0bf56906c0c
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-01-22 21:34:55 +0000

    lec4

---
+++ lec4.py
@@ -0,0 +1,38 @@
+"""
+lec 4 dict and tuple 
+"""
+
+# my_tuple = 'a','b','c','d','e'
+# print(my_tuple)
+
+# my_second_tuple = (my_tuple)
+# print(my_second_tuple)
+
+# not_a_tuple = ('a')
+# print(type(not_a_tuple))
+
+# is_a_tuple=('a',)
+# print(type(is_a_tuple))
+
+# print(my_tuple[:])
+
+my_car = {
+            'color':'red',
+            'maker':'toyota',
+            'year':2015
+        }
+# print(my_car['year'])
+
+# print(my_car.get('year'))
+
+my_car['model']='Corolla'
+
+print(my_car)
+
+my_car['year']=2020
+
+print(my_car)
+
+print(len(my_car))
+
+print('color' in my_car)
