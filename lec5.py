d3c3539e36b5b3687ca8b5099dc0716c04221981
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-01-24 00:27:06 +0000

    lec5 if statements

---
+++ lec5.py
@@ -0,0 +1,27 @@
+"""
+lec 5 if statement 
+"""
+
+# import this 
+
+# print(1+
+#      2)
+     
+# print([2,3,4,
+#         5,6,7])
+        
+# m = 1\
+#     +1
+
+# print(m)
+
+a = [1,2,3]
+
+b = [1,2,3]
+
+print(id([1,2,3]))
+print(id(a))
+print(id(b))
+
+print(a ==b)
+print(a is b)
