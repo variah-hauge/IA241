76400353a75cabd223a778ca9ba09879924e597b
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-01-27 04:13:59 +0000

    lec5 if statements

--- lec5.py
+++ lec5.py
@@ -15,13 +15,68 @@ lec 5 if statement
 
 # print(m)
 
-a = [1,2,3]
+# a = [1,2,3]
 
-b = [1,2,3]
+# b = [1,2,3]
 
-print(id([1,2,3]))
-print(id(a))
-print(id(b))
+# print(id([1,2,3]))
+# print(id(a))
+# print(id(b))
 
-print(a ==b)
-print(a is b)
+# print(a ==b)
+# print(a is b)
+
+# x= None 
+
+# print(id(x))
+# print(id(None))
+
+# print(x ==None)
+# print(x is None)
+
+# y = []
+# print(y == None)
+# print(y is None)
+
+# print(not '0')
+
+# print( 0 or -1 )
+
+# if 2>1:
+#     print('2>1')
+#     if 3>1:
+#         print('3>1')
+    
+#     if 2<=1:
+#         print('2<=1')
+    
+# print('not in the if block')
+
+# if 2<=1:
+#     print('2<=1')
+# else: 
+#     print('2>1')
+    
+# if 2<=2:
+#     print('2<=2')
+# else:
+#     primt('2>2')
+    
+# if 2<=1: 
+#     print('2<=1')
+    
+# elif 2<=2:
+#     print('2<=2')
+# else:
+#     print('2>1')
+
+if None: 
+    print(1)
+elif{}:
+    print(2)
+elif '0':
+    print(3)
+else:
+    print(4)
+    
+    
