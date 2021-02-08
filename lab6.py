3c2e17e40bc87d98316cb14b2a23258dc8aab7d8
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-02-08 05:23:26 +0000

    lab6

--- lab6.py
+++ lab6.py
@@ -2,57 +2,57 @@
 lab6 range function and for loop 
 '''
 
-# 3.1 
+#3.1 
 
-# for i in range(6):
-#     if i !=3:
-#         print(i)
+for i in range(6):
+    if i !=3:
+        print(i)
     
-# 3.2
+#3.2
 
-# result = 1
+result = 1
 
-# for i in range(1,6):
-#     result = result * i
+for i in range(1,6):
+    result = result * i
 
-# print(result)
+print(result)
 
-# 3.3
+#3.3
 
-# result = 0
+result = 0
 
-# for i in range (1,6):
-#     result = result + i
+for i in range (1,6):
+    result = result + i
 
-# print(result)
+print(result)
 
-# 3.4
+#3.4
 
-# result = 1
+result = 1
 
-# for i in range (3,9): 
-#     result = result * i
+for i in range (3,9): 
+    result = result * i
     
-# print(result)
+print(result)
 
-# 3.5
+#3.5
 
-# result = 1
+result = 1
 
-# for i in range (4,9): 
-#     result = result * i
+for i in range (4,9): 
+    result = result * i
     
-# print(result)
+print(result)
 
-# # 3.6
-# result = 0 
-# for word in 'this is my 6th string'.split(): 
-#     print(word)
-#     result = result + 1
+# #3.6
+result = 0 
+for word in 'this is my 6th string'.split(): 
+    print(word)
+    result = result + 1
     
-# print(result)
+print(result)
 
-# 3.7
+#3.7
 
 my_tweet = {
         "favorite_count":1138,
