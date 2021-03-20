6e21693814d897288defb83c2e4411bbea17f150
EC2 Default User <ec2-user@ip-172-31-7-37.ec2.internal>
2021-03-20 22:57:33 +0000

    lec9 class

--- lec9.py
+++ lec9.py
@@ -12,22 +12,21 @@ class car:
     def report(self): 
         return self.model, self.maker
     
-my_corolla = car('corolla')
+# my_corolla = car('corolla')
 
-my_highlander = car('highlander')
+# my_highlander = car('highlander')
 
-print(my_corolla.model)
+# print(my_corolla.model)
 
-my_corolla.maker = 'ford'
-print(my_corolla.report())
+# my_corolla.maker = 'ford'
+# print(my_corolla.report())
 
-print(my_corolla.maker)
-print(my_highlander.maker)
+# print(my_corolla.maker)
+# print(my_highlander.maker)
 
-print(my_corolla.model)
-print(my_highlander.model)
+# print(my_corolla.model)
+# print(my_highlander.model)
 
-import lec8
+# import lec8
 
-print(lec8.cal_f(5))
-
+# print(lec8.cal_f(5))
